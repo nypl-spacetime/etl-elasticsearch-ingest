611652ee2b4a3c41064d2a3f38c2f40d0fd48bdc
Bert Spaan <bert.spaan@gmail.com>
2017-04-28 12:00:52 -0400

    use ES aliases to create a new index, swap aliases, and then remove old index

--- elasticsearch-ingest.js
+++ elasticsearch-ingest.js
@@ -9,24 +9,34 @@ const elasticsearch = require('spacetime-db-elasticsearch')
 const DATASET = 'spacetime-graph'
 const STEP = 'aggregate'
 
-function ingest (config, dirs, tools, callback) {
-  const dir = dirs.getDir(DATASET, STEP)
-  const dataset = require(path.join(dir, `${DATASET}.dataset.json`))
+function addVersion (dataset, version) {
+  return `${dataset}-v${version}`
+}
+
+function getVersion (index) {
+  const match = /-v(\d+)$/.exec(index)
+  return parseInt(match[1])
+}
+
+function doehet (dataset, ndjsonStream, version, callback) {
+  const versionedDataset = Object.assign({}, dataset, {
+    id: addVersion(dataset.id, version)
+  })
 
   const datasetStream = H.of({
     type: 'dataset',
     action: 'create',
-    payload: dataset
+    payload: versionedDataset
   })
 
-  const objectStream = H(fs.createReadStream(path.join(dir, `${DATASET}.objects.ndjson`)))
+  const objectStream = H(ndjsonStream)
     .split()
     .compact()
     .map(JSON.parse)
     .map((object) => ({
       type: 'object',
       meta: {
-        dataset: DATASET
+        dataset: addVersion(DATASET, version)
       },
       action: 'create',
       payload: object
@@ -39,7 +49,46 @@ function ingest (config, dirs, tools, callback) {
     .nfcall([])
     .series()
     .errors(callback)
-    .done(callback)
+    .done(() => {
+      const indexNew = addVersion(DATASET, version)
+
+      if (version > 1) {
+        const indexOld = addVersion(DATASET, version - 1)
+
+        elasticsearch.updateAliases(indexOld, indexNew, DATASET, (err, resp) => {
+          if (err) {
+            callback(err)
+            return
+          }
+
+          elasticsearch.delete([indexOld], callback)
+        })
+      } else {
+        elasticsearch.putAlias(indexNew, DATASET, callback)
+      }
+    })
+}
+
+function ingest (config, dirs, tools, callback) {
+  const dir = dirs.getDir(DATASET, STEP)
+  const dataset = require(path.join(dir, `${DATASET}.dataset.json`))
+  const ndjsonStream = fs.createReadStream(path.join(dir, `${DATASET}.objects.ndjson`))
+
+  // Use alias to swap old + new index
+  // https://www.elastic.co/guide/en/elasticsearch/guide/current/index-aliases.html
+  elasticsearch.getAliasedIndex(DATASET, (err, index) => {
+    if (err) {
+      if (err.status === 404) {
+        // alias does not exist, set version to 1!
+        doehet(dataset, ndjsonStream, 1, callback)
+      } else {
+        callback(err)
+      }
+    } else {
+      const currentVersion = getVersion(index)
+      doehet(dataset, ndjsonStream, currentVersion + 1, callback)
+    }
+  })
 }
 
 // ==================================== API ====================================
